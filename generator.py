2a5c6b38c02eec3c7cab1c3b3761ed63462c84c1
TI-capacitor <danielbarreiro014@gmail.com>
2026-08-18 15:59:17 -0400

    Created script to generate qr codes based on url

--- generator.py
+++ generator.py
@@ -1,1 +1,16 @@
-import this
+import qrcode
+
+
+#Path were qr codes generated will be stored for now
+file_path = "C:\\Users\\test\\OneDrive - Inter American University of Puerto Rico - Bayamon Campus\\Desktop\\Interamericana\\2026-2027\\QR_GENERATOR\\qr_codes\\dantutoringqr.png"
+url = input("Enter your url: ").strip()
+
+print(url)
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR code was generated")
