253d45fdd39ae8dc5d8c1da5a886dd12db915945
Daniel Barreiro <danielbarreiro014@gmail.com>
2026-08-23 18:52:12 -0400

    Implement QR code generation functionality with dynamic file naming inside of class in another file.

--- Qrcode.py
+++ Qrcode.py
@@ -1,2 +1,17 @@
-class Qrcode:
-    pass
+import qrcode 
+
+class QR:
+    
+    def __init__(self,url,img_name):
+        self.url = url
+        self.img_name = img_name
+
+
+    def generate_qr(self, file_path):
+        qr = self.qrcode.QRCode()
+        qr.add_data(self.url)
+        img = qr.make_image()
+        img.save(file_path + self.img_name)
+
+    def success_message(self):
+        print("QR code generated successfully!")
--- generator.py
+++ generator.py
@@ -1,24 +1,10 @@
-import qrcode
-
-
+from Qrcode import QR
 #Path were qr codes generated will be stored for now
-file_path = "C:\\Users\\test\\OneDrive - Inter American University of Puerto Rico - Bayamon Campus\\Desktop\\Interamericana\\2026-2027\\QR_GENERATOR\\qr_codes\\"
+file_path = "C:\\Users\\Dani\\Desktop\\"
 pngName = input("Enter the name you want for your code (must end in .png)")
-file_path += pngName
 url = input("Enter your url: ").strip()
 
+qr_code = QR(url, pngName)
+qr_code.generate_qr(file_path)
+qr_code.success_message()
 
-#declaration of qr object of type 'qrcode' 
-qr = qrcode.QRCode()
-
-#use the object qr to add the url entered by user with the add_data() method
-qr.add_data(url)
-
-#.make_image() generates the image embedded with the url entered
-img = qr.make_image()
-
-#.save() method saves the 
-img.save(file_path)
-
-#successful run of code
-print("QR code was generated succesfullly")
